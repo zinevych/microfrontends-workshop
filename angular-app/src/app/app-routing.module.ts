711c34500333bd1d826b638fcc47c8e664e68d39
Aleks Zinevych <aleksanderzinewicz@gmail.com>
2019-07-28 15:00:04 +0300

    ng add single-spa-angular + additional configurations

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DogsComponent } from './dogs.component';
+import { APP_BASE_HREF } from '@angular/common'
 
 
 const routes: Routes = [
-  { path: '', component: DogsComponent },
+  { path: '**', component: DogsComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'},
+  ]
 })
 export class AppRoutingModule { }
